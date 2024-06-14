8baeb3c6c13fb38c03801c0ac965b5c7bc39acb1
olaola-0 <isholamyson@gmail.com>
2024-06-14 18:43:32 +0200

    Refactor trade producer service to use typed product_ids

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,4 +1,3 @@
-import os
 from typing import List
 
 from dotenv import find_dotenv, load_dotenv
@@ -9,19 +8,24 @@ load_dotenv(find_dotenv())
 
 
 class Config(BaseSettings):
-    kafka_broker_address: str = os.environ['KAFKA_BROKER_ADDRESS']
-    kafka_topic_name: str = os.environ['KAFKA_TOPIC_NAME']
+    kafka_broker_address: str
+    kafka_topic_name: str
     product_ids: List[str] = [
-    'BTC/USD',
-    'ETH/USD',
-    'ADA/USD',
-    'SOL/USD',
-    'DOT/USD',
-    'LUNA/USD',
-    'AVAX/USD',
-    'DOGE/USD',
-    'SHIB/USD',
-    'UNI/USD',
+        'BTC/USD',
+        #'ETH/USD',
+        #'ADA/USD',
+        #'SOL/USD',
+        #'DOT/USD',
+        #'LUNA/USD',
+        #'AVAX/USD',
+        #'DOGE/USD',
+        #'SHIB/USD',
+        #'UNI/USD',
     ]
+    # The mode of operation for the trade producer service (live or historical)
+    live_or_historical: str = 'live'
+    # The number of days of historical trade data to retrieve
+    last_n_days: int = 1
+
 
 config = Config()
--- services/trade_producer/src/main.py
+++ services/trade_producer/src/main.py
@@ -1,19 +1,22 @@
 """Description: Main entry point for the trade producer service."""
 
 # Gets msgs from a websocket and sends them to redpanda topic
-from time import sleep
 from typing import Dict, List
 
-from config import config
-
-# Import the KrakenWebsocketTradeAPI class from the kraken_api module
-from kraken_api import KrakenWebsocketTradeAPI
 from loguru import logger as logging
 from quixstreams import Application
 
+from src.config import config
+from src.kraken_api.rest import KrakenRestAPIMultipleProducts
+from src.kraken_api.websocket import KrakenWebsocketTradeAPI
+
 
 def produce_trades(
-    kafka_broker_address: str, kafka_topic_name: str, product_ids: List[str]
+    kafka_broker_address: str,
+    kafka_topic_name: str,
+    product_ids: List[str],
+    live_or_historical: str,
+    last_n_days: int,
 ) -> None:
     """
     Reads trades from Kraken websocket API and saves them to a Kafka topic.
@@ -22,39 +25,62 @@ def produce_trades(
         kafka_broker_address (str): The address of the Kafka broker.
         kafka_topic_name (str): The name of the Kafka topic.
         product_ids (List[str]): List of product IDs to subscribe to.
+        live_or_historical (str): The mode of operation for the trade producer service (live or historical).
+        last_n_days (int): The number of days of historical trade data to retrieve.
     Returns:
         None
     """
-    logging.info(f'Starting trade producer service for products: {product_ids}')
+    assert live_or_historical.strip() in {
+        'live',
+        'historical',
+    }, f'Invalid mode of operation: {live_or_historical}. Must be live or historical.'
+
     # Create an application. The application is the main entry point for interacting with the QuixStreams API
     app = Application(broker_address=kafka_broker_address)
 
     # The topic to which the trade events will be sent
     topic = app.topic(name=kafka_topic_name, value_serializer='json')
 
-    # Create an instance of the KrakenWebsocketTradeAPI class
-    kraken_api = KrakenWebsocketTradeAPI(product_ids=product_ids)
+    logging.info(f'Starting trade producer service for products: {product_ids}')
+
+    # Create an instance of the KrakenWebsocketTradeAPI class if the mode of operation is live
+    if live_or_historical == 'live':
+        kraken_api = KrakenWebsocketTradeAPI(product_ids=product_ids)
+    else:
+        # Create an instance of the KrakenRestAPI class if the mode of operation is historical
+        kraken_api = KrakenRestAPIMultipleProducts(
+            product_ids=product_ids, last_n_days=last_n_days
+        )
 
     # Create a Producer instance
     logging.info('Creating producer instance...')
     with app.get_producer() as producer:
         while True:
+            # Check if all historical trades have been produced
+            if kraken_api.is_done():
+                logging.info('All historical trades have been produced.')
+                break
+
             try:
                 # Get the trades from the Kraken API
                 trades_response: List[Dict] = kraken_api.get_trades()
-                logging.info(f'Received {len(trades_response)} trades from Kraken API.')
+                #logging.info(f'Received {len(trades_response)} trades from Kraken API.')
 
                 # Iterate over the trades and send them to the Kafka topic
                 for trade in trades_response:
                     # Serialize an event using the defined Topic
-                    message = topic.serialize(key=trade['product_id'], value=trade)
+                    message = topic.serialize(
+                        key=trade['product_id'],
+                        value=trade,
+                        timestamp_ms=trade['time'] * 1000,  # Convert the timestamp to milliseconds
+                    )
 
                     # Produce a message into the Kafka topic
                     producer.produce(
                         topic=topic.name, value=message.value, key=message.key
                     )
-                    logging.info(f'Trade produced: {trade}')
-                sleep(1)
+                    logging.info(f'Produced {trade["product_id"]}: {trade}')
+                # sleep(1)
             except Exception as e:
                 # Log the full error message that occurs during the trade production process
                 logging.error(f'Error producing trades: {str(e)}')
@@ -66,7 +92,9 @@ if __name__ == '__main__':
         produce_trades(
             kafka_broker_address=config.kafka_broker_address,
             kafka_topic_name=config.kafka_topic_name,
-            product_ids=config.product_ids
+            product_ids=config.product_ids,
+            live_or_historical=config.live_or_historical,
+            last_n_days=config.last_n_days,
         )
     except KeyboardInterrupt:
         logging.info('Trade producer service stopped.')
