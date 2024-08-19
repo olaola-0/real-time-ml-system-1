af3d8eaad46232a8d348774544b912c19bdcc703
olaola-0 olaola-0 <isholamyson@gmail.com>
2024-08-19 20:23:45 +0000

    Edited 'setup_live_config.sh'

--- services/trade_producer/setup_live_config.sh
+++ services/trade_producer/setup_live_config.sh
@@ -1,5 +1,5 @@
 # These are the environment variables needed to run the trade_producer in live mode in Quix Cloud
 
 export KAFKA_TOPIC=trade
-export PRODUCT_IDS=["BTC/USD", "ETH/EUR", "ETH/USD"]
+export PRODUCT_IDS='["BTC/USD", "ETH/EUR", "ETH/USD"]'
 export LIVE_OR_HISTORICAL=live
